5a5e67aaa3382ad759a8d4f4f31d791d9fb0cbca
Julio Chazari <julio.chazari@gmail.com>
2020-05-26 23:05:55 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -7,7 +7,7 @@ end
 def consolidate_cart(cart)
 
   #cart_keys = cart.flat_map{|item| item[:item]}
-  cart.inject({}) do |index, object|
+  cart.inject([]) do |index, object|
     binding.pry
   end
   binding.pry
