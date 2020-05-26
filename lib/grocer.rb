7128f243407f5cdc90a7eab3904c72133ba3f8f3
Julio Chazari <julio.chazari@gmail.com>
2020-05-26 22:57:25 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -7,12 +7,6 @@ end
 def consolidate_cart(cart)
 
   cart_keys = cart.flat_map{|item| item[:item]}
-  test_cart = [
-    {:item => "AVOCADO", :price => 3.00, :clearance => true },
-    {:item => "AVOCADO", :price => 3.00, :clearance => true },
-    {:item => "KALE", :price => 3.00, :clearance => false}
-  ]
-  result = test_cart.flat_map{|item| item[:item]}
   binding.pry
 
 end
