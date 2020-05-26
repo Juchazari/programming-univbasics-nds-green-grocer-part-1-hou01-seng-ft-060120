fec1dfdb7b29915798e29fba974b5d0567652a5a
Julio Chazari <julio.chazari@gmail.com>
2020-05-26 22:56:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,18 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+require 'pry'
 
+def find_item_by_name_in_collection(name, collection)
+  collection.find {|i| i[:item] == name}
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+
+  cart_keys = cart.flat_map{|item| item[:item]}
+  test_cart = [
+    {:item => "AVOCADO", :price => 3.00, :clearance => true },
+    {:item => "AVOCADO", :price => 3.00, :clearance => true },
+    {:item => "KALE", :price => 3.00, :clearance => false}
+  ]
+  result = test_cart.flat_map{|item| item[:item]}
+  binding.pry
 
 end
-
-
-  
